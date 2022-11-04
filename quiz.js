008f67f0ffa9b2139ffd21e237a13a1606095810
Maicon Douglas <maicondsoledade@gmail.com>
2022-11-04 10:46:55 -0300

    ajuste

---
+++ quiz.js
@@ -0,0 +1,599 @@
+var questoes = {
+    q1: {
+
+        q1enum: "Qual é o tipo do medidor?",
+      
+        q1rescorreta: "Medidor eletromecânico",
+      
+        q1_1: "Medidor eletromecânico",
+      
+        q1_2: "Medidor mecânico",
+      
+        q1_3: "Medidor eletrônico",
+      
+        q1image: "images/IMG_7970.png",
+      
+        sorteado: false,
+      
+      },
+      q2: {
+      
+      q2enum: "Qual é o tipo do medidor?",
+      
+      q2rescorreta: "Medidor eletromecânico",
+      
+      q2_1: "Medidor eletromecânico",
+      
+      q2_2: "Medidor mecânico",
+      
+      q2_3: "Medidor eletrônico",
+      
+      q2image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      
+      q3: {
+      
+      q3enum: "Qual é o tipo do medidor?",
+      
+      q3rescorreta: "Medidor eletromecânico",
+      
+      q3_1: "Medidor eletromecânico",
+      
+      q3_2: "Medidor mecânico",
+      
+      q3_3: "Medidor eletrônico",
+      
+      q3image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q4: {
+      
+      q4enum: "Qual é o tipo do medidor?",
+      
+      q4rescorreta: "Medidor eletromecânico",
+      
+      q4_1: "Medidor eletromecânico",
+      
+      q4_2: "Medidor mecânico",
+      
+      q4_3: "Medidor eletrônico",
+      
+      q4image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q5: {
+      
+      q5enum: "Qual é o tipo do medidor?",
+      
+      q5rescorreta: "Medidor eletromecânico",
+      
+      q5_1: "Medidor eletromecânico",
+      
+      q5_2: "Medidor mecânico",
+      
+      q5_3: "Medidor eletrônico",
+      
+      q5image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q6: {
+      
+      q6enum: "Qual é o tipo do medidor?",
+      
+      q6rescorreta: "Medidor eletromecânico",
+      
+      q6_1: "Medidor eletromecânico",
+      
+      q6_2: "Medidor mecânico",
+      
+      q6_3: "Medidor eletrônico",
+      
+      q6image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q7: {
+      
+      q7enum: "Qual é o tipo do medidor?",
+      
+      q7rescorreta: "Medidor eletromecânico",
+      
+      q7_1: "Medidor eletromecânico",
+      
+      q7_2: "Medidor mecânico",
+      
+      q7_3: "Medidor eletrônico",
+      
+      q7image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q8: {
+      
+      q8enum: "Qual é o tipo do medidor?",
+      
+      q8rescorreta: "Medidor eletromecânico",
+      
+      q8_1: "Medidor eletromecânico",
+      
+      q8_2: "Medidor mecânico",
+      
+      q8_3: "Medidor eletrônico",
+      
+      q8image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q9: {
+      
+      q9enum: "Qual é o tipo do medidor?",
+      
+      q9rescorreta: "Medidor eletromecânico",
+      
+      q9_1: "Medidor eletromecânico",
+      
+      q9_2: "Medidor mecânico",
+      
+      q9_3: "Medidor eletrônico",
+      
+      q9image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q10: {
+      
+      q10enum: "Qual é o tipo do medidor?",
+      
+      q10rescorreta: "Medidor eletromecânico",
+      
+      q10_1: "Medidor eletromecânico",
+      
+      q10_2: "Medidor mecânico",
+      
+      q10_3: "Medidor eletrônico",
+      
+      q10image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q11: {
+      
+      q11enum: "Qual é o tipo do medidor?",
+      
+      q11rescorreta: "Medidor eletromecânico",
+      
+      q11_1: "Medidor eletromecânico",
+      
+      q11_2: "Medidor mecânico",
+      
+      q11_3: "Medidor eletrônico",
+      
+      q11image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q12: {
+      
+      q12enum: "Qual é o tipo do medidor?",
+      
+      q12rescorreta: "Medidor eletromecânico",
+      
+      q12_1: "Medidor eletromecânico",
+      
+      q12_2: "Medidor mecânico",
+      
+      q12_3: "Medidor eletrônico",
+      
+      q12image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q13: {
+      
+      q13enum: "Qual é o tipo do medidor?",
+      
+      q13rescorreta: "Medidor eletromecânico",
+      
+      q13_1: "Medidor eletromecânico",
+      
+      q13_2: "Medidor mecânico",
+      
+      q13_3: "Medidor eletrônico",
+      
+      q13image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q14: {
+      
+      q14enum: "Qual é o tipo do medidor?",
+      
+      q14rescorreta: "Medidor eletromecânico",
+      
+      q14_1: "Medidor eletromecânico",
+      
+      q14_2: "Medidor mecânico",
+      
+      q14_3: "Medidor eletrônico",
+      
+      q14image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q15: {
+      
+      q15enum: "Qual é o tipo do medidor?",
+      
+      q15rescorreta: "Medidor eletromecânico",
+      
+      q15_1: "Medidor eletromecânico",
+      
+      q15_2: "Medidor mecânico",
+      
+      q15_3: "Medidor eletrônico",
+      
+      q15image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q16: {
+      
+      q16enum: "Qual é o tipo do medidor?",
+      
+      q16rescorreta: "Medidor eletromecânico",
+      
+      q16_1: "Medidor eletromecânico",
+      
+      q16_2: "Medidor mecânico",
+      
+      q16_3: "Medidor eletrônico",
+      
+      q16image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q17: {
+      
+      q17enum: "Qual é o tipo do medidor?",
+      
+      q17rescorreta: "Medidor eletromecânico",
+      
+      q17_1: "Medidor eletromecânico",
+      
+      q17_2: "Medidor mecânico",
+      
+      q17_3: "Medidor eletrônico",
+      
+      q17image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q18: {
+      
+      q18enum: "Qual é o tipo do medidor?",
+      
+      q18rescorreta: "Medidor eletromecânico",
+      
+      q18_1: "Medidor eletromecânico",
+      
+      q18_2: "Medidor mecânico",
+      
+      q18_3: "Medidor eletrônico",
+      
+      q18image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q19: {
+      
+      q19enum: "Qual é o tipo do medidor?",
+      
+      q19rescorreta: "Medidor eletromecânico",
+      
+      q19_1: "Medidor eletromecânico",
+      
+      q19_2: "Medidor mecânico",
+      
+      q19_3: "Medidor eletrônico",
+      
+      q19image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q20: {
+      
+      q20enum: "Qual é o tipo do medidor?",
+      
+      q20rescorreta: "Medidor eletromecânico",
+      
+      q20_1: "Medidor eletromecânico",
+      
+      q20_2: "Medidor mecânico",
+      
+      q20_3: "Medidor eletrônico",
+      
+      q20image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q21: {
+      
+      q21enum: "Qual é o tipo do medidor?",
+      
+      q21rescorreta: "Medidor eletromecânico",
+      
+      q21_1: "Medidor eletromecânico",
+      
+      q21_2: "Medidor mecânico",
+      
+      q21_3: "Medidor eletrônico",
+      
+      q21image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q22: {
+      
+      q22enum: "Qual é o tipo do medidor?",
+      
+      q22rescorreta: "Medidor eletromecânico",
+      
+      q22_1: "Medidor eletromecânico",
+      
+      q22_2: "Medidor mecânico",
+      
+      q22_3: "Medidor eletrônico",
+      
+      q22image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q23: {
+      
+      q23enum: "Qual é o tipo do medidor?",
+      
+      q23rescorreta: "Medidor eletromecânico",
+      
+      q23_1: "Medidor eletromecânico",
+      
+      q23_2: "Medidor mecânico",
+      
+      q23_3: "Medidor eletrônico",
+      
+      q23image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q24: {
+      
+      q24enum: "Qual é o tipo do medidor?",
+      
+      q24rescorreta: "Medidor eletromecânico",
+      
+      q24_1: "Medidor eletromecânico",
+      
+      q24_2: "Medidor mecânico",
+      
+      q24_3: "Medidor eletrônico",
+      
+      q24image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q25: {
+      
+      q25enum: "Qual é o tipo do medidor?",
+      
+      q25rescorreta: "Medidor eletromecânico",
+      
+      q25_1: "Medidor eletromecânico",
+      
+      q25_2: "Medidor mecânico",
+      
+      q25_3: "Medidor eletrônico",
+      
+      q25image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q26: {
+      
+      q26enum: "Qual é o tipo do medidor?",
+      
+      q26rescorreta: "Medidor eletromecânico",
+      
+      q26_1: "Medidor eletromecânico",
+      
+      q26_2: "Medidor mecânico",
+      
+      q26_3: "Medidor eletrônico",
+      
+      q26image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q27: {
+      
+      q27enum: "Qual é o tipo do medidor?",
+      
+      q27rescorreta: "Medidor eletromecânico",
+      
+      q27_1: "Medidor eletromecânico",
+      
+      q27_2: "Medidor mecânico",
+      
+      q27_3: "Medidor eletrônico",
+      
+      q27image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q28: {
+      
+      q28enum: "Qual é o tipo do medidor?",
+      
+      q28rescorreta: "Medidor eletromecânico",
+      
+      q28_1: "Medidor eletromecânico",
+      
+      q28_2: "Medidor mecânico",
+      
+      q28_3: "Medidor eletrônico",
+      
+      q28image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q29: {
+      
+      q29enum: "Qual é o tipo do medidor?",
+      
+      q29rescorreta: "Medidor eletromecânico",
+      
+      q29_1: "Medidor eletromecânico",
+      
+      q29_2: "Medidor mecânico",
+      
+      q29_3: "Medidor eletrônico",
+      
+      q29image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q30: {
+      
+      q30enum: "Qual é o tipo do medidor?",
+      
+      q30rescorreta: "Medidor eletromecânico",
+      
+      q30_1: "Medidor eletromecânico",
+      
+      q30_2: "Medidor mecânico",
+      
+      q30_3: "Medidor eletrônico",
+      
+      q30image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q31: {
+      
+      q31enum: "Qual é o tipo do medidor?",
+      
+      q31rescorreta: "Medidor eletromecânico",
+      
+      q31_1: "Medidor eletromecânico",
+      
+      q31_2: "Medidor mecânico",
+      
+      q31_3: "Medidor eletrônico",
+      
+      q31image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q32: {
+      
+      q32enum: "Qual é o tipo do medidor?",
+      
+      q32rescorreta: "Medidor eletromecânico",
+      
+      q32_1: "Medidor eletromecânico",
+      
+      q32_2: "Medidor mecânico",
+      
+      q32_3: "Medidor eletrônico",
+      
+      q32image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q33: {
+      
+      q33enum: "Qual é o tipo do medidor?",
+      
+      q33rescorreta: "Medidor eletromecânico",
+      
+      q33_1: "Medidor eletromecânico",
+      
+      q33_2: "Medidor mecânico",
+      
+      q33_3: "Medidor eletrônico",
+      
+      q33image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q34: {
+      
+      q34enum: "Qual é o tipo do medidor?",
+      
+      q34rescorreta: "Medidor eletromecânico",
+      
+      q34_1: "Medidor eletromecânico",
+      
+      q34_2: "Medidor mecânico",
+      
+      q34_3: "Medidor eletrônico",
+      
+      q34image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      q35: {
+      
+      q35enum: "Qual é o tipo do medidor?",
+      
+      q35rescorreta: "Medidor eletromecânico",
+      
+      q35_1: "Medidor eletromecânico",
+      
+      q35_2: "Medidor mecânico",
+      
+      q35_3: "Medidor eletrônico",
+      
+      q35image: "images/IMG_7970.png",
+      
+      sorteado: false,
+      
+      },
+      
+}
